38779f49460a59e2f2def8f3aabf72f7a3a87c13
tanayg24 <tanayr.24.bcds@acharya.ac.in>
2026-09-09 16:42:54 +0530

    Time: 2 ms (68.79%), Space: 45.2 MB (72.32%) - LeetHub

---
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+        int m = dungeon.length;
+        int n = dungeon[0].length;
+
+        int[][] dp = new int[m][n];
+
+        // Princess room
+        dp[m - 1][n - 1] =
+            Math.max(1, 1 - dungeon[m - 1][n - 1]);
+
+        // Last row
+        for (int j = n - 2; j >= 0; j--) {
+            dp[m - 1][j] =
+                Math.max(1, dp[m - 1][j + 1] - dungeon[m - 1][j]);
+        }
+
+        // Last column
+        for (int i = m - 2; i >= 0; i--) {
+            dp[i][n - 1] =
+                Math.max(1, dp[i + 1][n - 1] - dungeon[i][n - 1]);
+        }
+
+        // Remaining cells
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+
+                int nextHealth =
+                    Math.min(dp[i + 1][j], dp[i][j + 1]);
+
+                dp[i][j] =
+                    Math.max(1, nextHealth - dungeon[i][j]);
+            }
+        }
+
+        return dp[0][0];
+    }
+}
