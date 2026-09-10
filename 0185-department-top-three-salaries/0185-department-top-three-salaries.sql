31b629ab00f26bd2208b11128ee65e8ab5382ed0
tanayg24 <tanayr.24.bcds@acharya.ac.in>
2026-09-10 23:34:44 +0530

    Time: 1048 ms (66.29%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,18 @@
+SELECT
+    Department,
+    Employee,
+    Salary
+FROM (
+    SELECT
+        d.name AS Department,
+        e.name AS Employee,
+        e.salary AS Salary,
+        DENSE_RANK() OVER (
+            PARTITION BY e.departmentId
+            ORDER BY e.salary DESC
+        ) AS salary_rank
+    FROM Employee e
+    JOIN Department d
+        ON e.departmentId = d.id
+) ranked
+WHERE salary_rank <= 3;
