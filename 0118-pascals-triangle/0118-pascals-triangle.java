033a49d0fb227b7bf2843d7b2adbf50d99f0041e
tanayg24 <tanayr.24.bcds@acharya.ac.in>
2026-09-12 20:27:39 +0530

    Time: 1 ms (95.36%), Space: 43.3 MB (85.79%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -7,18 +7,15 @@ class Solution {
         for (int i = 0; i < numRows; i++) {
             List<Integer> row = new ArrayList<>();
 
-            // First element
             row.add(1);
 
-            // Middle elements
             for (int j = 1; j < i; j++) {
                 int value = result.get(i - 1).get(j - 1)
-                          + result.get(i - 1).get(j);
+                           + result.get(i - 1).get(j);
 
                 row.add(value);
             }
 
-            // Last element
             if (i > 0) {
                 row.add(1);
             }
