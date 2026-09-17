0a3c6b668c0b924705dc4e9069640405be907b5d
tanayg24 <tanayr.24.bcds@acharya.ac.in>
2026-09-18 00:19:48 +0530

    Time: 0 ms (100%), Space: 42.8 MB (65.42%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -0,0 +1,18 @@
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        // Swap left and right
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        // Invert subtrees
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
