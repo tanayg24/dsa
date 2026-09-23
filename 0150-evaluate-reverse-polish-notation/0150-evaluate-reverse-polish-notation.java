5c7d67b27e90341d7b55f36aeb4a22c9ad7cb0f6
tanayg24 <tanayr.24.bcds@acharya.ac.in>
2026-09-23 19:09:41 +0530

    Time: 7 ms (37.84%), Space: 45.1 MB (89.68%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+
+            if (token.equals("+") ||
+                token.equals("-") ||
+                token.equals("*") ||
+                token.equals("/")) {
+
+                int b = stack.pop();
+                int a = stack.pop();
+
+                int result = 0;
+
+                if (token.equals("+")) {
+                    result = a + b;
+                } else if (token.equals("-")) {
+                    result = a - b;
+                } else if (token.equals("*")) {
+                    result = a * b;
+                } else {
+                    result = a / b;
+                }
+
+                stack.push(result);
+
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+}
